394fb9d3341b025542b0619b5ab668da62be8f19
Balkrishna Singh <balkrishna76.jspl@gmail.com>
2021-10-03 03:21:47 +0545

    Example2: Learning Derived Data Types

--- JavaExapmles/src/Example2.java
+++ JavaExapmles/src/Example2.java
@@ -1,5 +1,24 @@
 public class Example2 {
     public static void main(String[] args) {
-        System.out.println("hello, World");
+        /* Creating variable using Derived Data Types from Primitive such as:
+         *
+         * double :  To Store Float value like 2.0, 1.2 etc. >> Precision Number
+         * string : To Store sequence of character Value like "abc", "ABc", "aBc" etc.
+         *
+         * Let's take example of student and store their related info
+         * */
+        String firstName = "Balkrishna";//Derived
+        String lastName = "Singh";//Derived
+        int Age = 25; //Primitive
+        double gpa = 3.45; //Derived
+        String sex = "Male"; //Derived
+        boolean isPassed = true; //Primitive
+
+        //Now Printing those value stored on variables
+        System.out.println("Full Name : "+firstName+" "+lastName+".");
+        System.out.println("Age of Student is " + Age + " year.");
+        System.out.println("GPA in Bachelor is " + gpa+".");
+        System.out.println("Sex : " + sex+".");
+        System.out.println("isPassed : " + isPassed);
     }
 }
